15ad2462370e6c6f54560e946c9c5f24f849a5ed
Tempron <43298787+Tempron@users.noreply.github.com>
2022-04-12 12:02:58 +0200

    Second commit

--- t1.c
+++ t1.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+void buf_in(unsigned char* buffer) {
+    const unsigned int size = 16;
+    for (int i = 0; i < size; i++) {
+        printf("%d ", buffer[i]);
+
+    }
+}
+
 int main() {
-    printf("Hello, World!\n");
+    unsigned char buffer[16] = {0};
+    buf_in(buffer);
     return 0;
 }
