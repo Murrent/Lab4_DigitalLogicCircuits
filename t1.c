01eee72d68d51a442a5c0fb3f1dcb29a45e3d4fa
marvih202 <marvih202@studentmail.oru.se>
2022-04-13 21:32:20 +0200

    backup

--- t1.c
+++ t1.c
@@ -1,15 +1,70 @@
 #include <stdio.h>
+#include <fcntl.h>
 
-void buf_in(unsigned char* buffer) {
-    const unsigned int size = 16;
-    for (int i = 0; i < size; i++) {
-        printf("%d ", buffer[i]);
+const unsigned int length = 16;
+unsigned char buffer[16] = {0};
+unsigned int position = 0;
 
+const char *source = "test.txt";
+int sourceFile;
+const char *dest = "destination.txt";
+int destFile;
+
+unsigned int buf_in() {
+    unsigned int index = position % length;
+    if (index == 0)
+        read(sourceFile, buffer, length);
+    if (position == filelength(sourceFile))
+        return EOF;
+    return buffer[index];
+}
+
+void readAndPrint(unsigned int readLength) {
+    printf("Read:\n");
+    for (int i = 0; i < readLength; i++) {
+        position = i;
+        unsigned int byte = buf_in();
+        printf("%c", byte);
+        if (byte == EOF) break;
     }
+    printf("\n");
+}
+
+void buf_flush() {
+    unsigned int count = position % length;
+    if (count == 0)
+        count = length;
+    printf("Writing %s %d %d \n", buffer, position, count);
+    write(destFile, buffer, count);
+}
+
+void buf_out(unsigned char byte) {
+    unsigned int index = position % length;
+    printf("buf_out %d\n", index);
+    if (index == 0 && position != 0)
+        buf_flush();
+    buffer[index] = byte;
+}
+
+void copy(unsigned int readLength) {
+    printf("Read:\n");
+    for (int i = 0; i < readLength; i++) {
+        position = i;
+        unsigned int byte = buf_in();
+        printf("bytes %s\n", buffer);
+        if (byte == EOF) break;
+        buf_out(byte);
+    }
+    buf_flush();
+    printf("\n");
 }
 
 int main() {
-    unsigned char buffer[16] = {0};
-    buf_in(buffer);
+    sourceFile = open(source, O_RDONLY);
+    destFile = open(dest, O_WRONLY);
+    copy(100);
+
+    close(destFile);
+    close(sourceFile);
     return 0;
 }
